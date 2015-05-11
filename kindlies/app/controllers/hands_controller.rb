f9fc3cb0b8e4b3f96f99d6734c138a4971814bb3
Tyler Geneva <ctg55@yahoo.com>
2015-05-11 18:51:35 -0500

    posting messages

--- kindlies/app/controllers/hands_controller.rb
+++ kindlies/app/controllers/hands_controller.rb
@@ -1,14 +1,19 @@
 class HandsController < ApplicationController
+
+  def new
+    @hand = Hand.new
+  end
+
   def create
-    hand = Hand.create( hand_params )
+    @hand = Hand.create( hand_params )
     # hand.user = current_user
     # hand.save
-    redirect_to "/index"
+    redirect_to "/users"
   end
 
   def destroy
     Hand.destroy(params[:id])
-    redirect_to "/index"
+    redirect_to "/"
   end
 
   private
