8c05d46a50261690137147250c0f5d549a77923c
Tyler Geneva <ctg55@yahoo.com>
2015-05-11 18:08:15 -0500

    migration

--- kindlies/db/migrate/20150511225044_create_hands.rb
+++ kindlies/db/migrate/20150511225044_create_hands.rb
@@ -1,6 +1,10 @@
 class CreateHands < ActiveRecord::Migration
   def change
     create_table :hands do |t|
+      t.string :title
+      t.string :message
+      t.references :user, index: true, foreign_key: true
+      t.integer :rank
 
       t.timestamps null: false
     end
