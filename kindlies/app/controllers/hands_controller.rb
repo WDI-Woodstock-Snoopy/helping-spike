ba447ff692d24c73d569eccdee758a69c73b5299
Tyler Geneva <ctg55@yahoo.com>
2015-05-13 10:54:11 -0500

    reverts to previous commit

--- kindlies/app/controllers/hands_controller.rb
+++ kindlies/app/controllers/hands_controller.rb
@@ -45,7 +45,7 @@ class HandsController < ApplicationController
   private
 
   def hand_params
-    params.require(:hand).permit(:message, :lat, :long, :title, :user_id, :score, :get_likes)
+    params.require(:hand).permit(:message, :lat, :long, :title, :user_id, :score, :get_likes, :gallery_id, :name, :image)
   end
 
 end
--- kindlies/app/models/hand.rb
+++ kindlies/app/models/hand.rb
@@ -1,8 +1,10 @@
 class Hand < ActiveRecord::Base
 
   #WE COULD PUT VALIDATIONS HERE.....
+
     belongs_to :user
     acts_as_votable
+
     def to_s
       self.title
     end
