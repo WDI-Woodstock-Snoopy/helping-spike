cfd5e54740cac4e884b3f8a654d426cbf1275dd3
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-13 22:27:16 -0500

    moves document.ready to app.js file

--- kindlies/app/assets/javascripts/app/app.js
+++ kindlies/app/assets/javascripts/app/app.js
@@ -1,1 +1,27 @@
-$('#display-hot').click()
+//WHEN THE DOCUMENT IS READY....SHOW THE FORM AND ALLOW USER TO SUBMIT CONTENT.
+$(document).ready(function(){
+
+  getNewDeeds();
+  getHotDeeds();
+  heyYall();
+
+  $( "#new_hand" ).hide();
+  getCoords(function(){
+    $( "#new_hand" ).fadeIn(500);
+  });
+
+  $( "#new_hand" ).submit(function( event ) {
+  // Stop form from submitting normally
+  event.preventDefault();
+  // Get some values from elements on the page:
+  summary = $( "#new_hand" ).find( "input[name='hand[title]']" ).val();
+  content = $( "#new_hand" ).find( "input[name='hand[message]']" ).val();
+  token = $( "#new_hand" ).find( "input[name='authenticity_token']" ).val();
+  image = $("#new_hand").find("input[name='hand[image]']").val();
+  // Send the data using post
+  submitData();
+
+  summary = $( "#new_hand" ).find( "input[name='hand[title]']" ).val("");
+  content = $( "#new_hand" ).find( "input[name='hand[message]']" ).val("");
+  })
+})
