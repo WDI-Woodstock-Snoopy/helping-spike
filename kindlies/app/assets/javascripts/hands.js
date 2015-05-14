b783cfeedefb434a4af4e8aaabae54b00b5818a0
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-14 09:39:43 -0500

    blah

--- kindlies/app/assets/javascripts/hands.js
+++ kindlies/app/assets/javascripts/hands.js
@@ -1,5 +1,25 @@
 //*****VIEW FUNCTIONS*****
 
+//DECLARES TIME/DATE VARS
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var display = $('#greetings')
+
+//DISPLAYS TIME-BASED GREETING TO CURRENT_USER
+function heyYall(){
+  if (hourNow > 18){
+    greeting = 'Good Evening, ';
+  } else if (hourNow > 12){
+    greeting = 'Good Afternoon, ';
+  } else if (hourNow > 0){
+    greeting = 'Good Morning, ';
+  } else {
+    greeting = 'Welcome, ';
+  }
+  display.prepend(greeting);
+}
+
 //CREATING VIEWS
 function getNewDeeds(){
   $.ajax({
