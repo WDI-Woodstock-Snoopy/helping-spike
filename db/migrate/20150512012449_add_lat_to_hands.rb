198d1b175b3d70c944f842c8f894dc5c0fdfc436
Tyler Geneva <ctg55@yahoo.com>
2015-05-15 17:11:40 -0500

    repairs migrations

--- db/migrate/20150512012449_add_lat_to_hands.rb
+++
@@ -1,5 +0,0 @@
-class AddLatToHands < ActiveRecord::Migration
-  def change
-    add_column :hands, :lat, :string
-  end
-end
--- db/migrate/20150512012508_add_long_to_hands.rb
+++
@@ -1,5 +0,0 @@
-class AddLongToHands < ActiveRecord::Migration
-  def change
-    add_column :hands, :long, :string
-  end
-end
