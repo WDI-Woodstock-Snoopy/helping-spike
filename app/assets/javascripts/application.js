cea159be15bdfe07775b9f627018864a761568a1
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-06-02 16:10:41 -0500

    cleans up code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,14 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require underscore
-//= require backbone
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
-//= require_tree ./views
+//= require_trlojee ./views
 //= require_tree ./routers
-//= require_tree ../../../vendor/assets/javascripts/
 //= require_tree ../../../vendor/assets/javascripts/
 //= require underscore
 //= require backbone
